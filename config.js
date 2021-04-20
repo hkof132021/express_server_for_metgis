3b8748202197459fdcd16322a1fa51637fc5a64e
billchan <billchan@hko.hksarg>
2021-04-20 14:56:18 +0800

    switch dotenv to config js, dynamic serve dir

---
+++ config.js
@@ -0,0 +1,31 @@
+// config.js
+var dict = [];
+env_setting = 'dev'
+//environment = 'pro'
+
+if(env_setting == 'dev'){
+    dict.push({
+        index:   "/images",
+        path: "/home/billchan/node_server/images"
+    });
+    dict.push({
+        index:   "/images_temp",
+        path: "/home/billchan/node_server/addon_data"
+    });
+}
+else if(env_setting == 'pro'){
+    dict.push({
+        index:   "/images",
+        path: "/home/cfodev/workspace/express_server/images"
+
+    });
+}
+const config = {
+    app: {
+      port: 8085,
+      host:'0.0.0.0',
+      content : dict
+    }
+   };
+   
+module.exports = config;
--- express_server.js
+++ express_server.js
@@ -7,18 +7,18 @@ var cors = require('cors');
 var morgan = require('morgan')
 var fs = require('fs');
 var app = express();
-var dotenv = require('dotenv')
-
-
+var config = require('./config');
+serve_dirs(config.app.content)
 // import other modules
 var api_controller = require('./controller/api_controller.js');
 var test_controller = require('./controller/test_controller.js');
 
 
+
 // ---------------------------   Get env variable  -----------------
-const envConfig = dotenv.parse(fs.readFileSync('.env'))
-var port = envConfig['port']
-var host = envConfig['host']
+
+var port = config.app.port
+var host = config.app.host
 
 
 // create a write stream (in append mode)
@@ -31,7 +31,6 @@ app.use(morgan('combined', {stream: accessLogStream}))
 const logger = function (req, res, next) {
     var url = req.protocol + '://' + req.get('host') + req.originalUrl;
     console.log('---- Calling ' +　url);
-    
     next();
   }
 // ---------------------------   APP init  -----------------
@@ -47,23 +46,22 @@ var test_router = express.Router();
 app.use('/test',test_router);
 app.use('/api',api_router);
 app.use(logger);
-options = {
-    'icons': true, 
-    stylesheet : 'public/style.css'
-}
 
-var directory = "/home/billchan/node_server/images"  // express server post
-var directory2 = "/home/billchan/node_server/addon_data"
-
-// http://protus:8085/images
-app.use('/images', serveStatic(directory), serveIndex(directory, options))
-app.use('/images_temp', serveStatic(directory2), serveIndex(directory2, options))
-
-// curl -X POST "http://protus:8085/api/upload"
 api_router.post('/upload', api_controller.post_image);
 test_router.get('/hello', test_controller.hello)
 
 
+function serve_dirs(content) { 
+    options = {
+        'icons': true, 
+        stylesheet : 'public/style.css'
+    }
+    console.log("### Directories to be served ... --- ")
+    for( key in content){
+        console.log(content[key]['index'] + " : " + content[key]['path']);
+        app.use(content[key]['index'], serveStatic(content[key]['path']), serveIndex(content[key]['path'], options))
+    }
+};
 
 // Start server
 var server = app.listen(port, host,  () => {
